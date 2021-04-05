6106d0134f5efc44b4704628aad990eca68c33ba
balkon16 <pawel.k.lonca@gmail.com>
2021-04-05 15:52:40 +0200

    zadanie 4

--- src/zadanie04/zadanie4.scala
+++ src/zadanie04/zadanie4.scala
@@ -3,8 +3,12 @@ package zadanie04
 object zadanie4 {
   def main(args: Array[String]): Unit = {
     val productPriceMap = Map("jajka" -> 6.99, "chleb" -> 3.5, "sok" -> 4.99, "jabłka" -> 3.05, "gruszki" -> 4.99, "ser" -> 23.99)
-    println(productPriceMap.get("ABC"))
+    println(showValue(productPriceMap.get("bułki")))
+    println(showValue(productPriceMap.get("jajka")))
+  }
+
+  def showValue(x : Option[Double]): String = x match {
+    case Some(s) => "Cena to " + s + '.'
+    case None => "Cena nieznana."
   }
 }
-
-// TODO: zrobić zadanie 4
