8455f1f575239ab47a78181c1f064bee8b3e24d6
balkon16 <pawel.k.lonca@gmail.com>
2021-04-05 18:28:53 +0200

    zadanie 2

--- src/zadanie02/zadanie2.scala
+++ src/zadanie02/zadanie2.scala
@@ -1,20 +1,14 @@
-//package zadanie02
-//
-//object zadanie2 {
-//  def main(args: Array[String]) {
-//    val productPriceMap = Map("jajka" -> 6.99, "chleb" -> 3.5, "sok" -> 4.99, "jabłka" -> 3.05, "gruszki" -> 4.99, "ser" -> 23.99)
-////    productPriceMap map((product, price) => (product, PriceEditor.decreasePrice(price)))
-//    var newProductPriceMap = productPriceMap.clone()
-//    println(newProductPriceMap)
-////    val newProductPriceMap = productPriceMap.view mapValues PriceEditor.decreasePrice
-////    println(newProductPriceMap)
-//  }
-//}
-//
-//object PriceEditor {
-//  def decreasePrice(price: Double): Double = {
-//    price*0.9
-//  }
-//}
+package zadanie02
 
-// TODO: zrobić zadanie nr 2
+object zadanie2 {
+  def main(args: Array[String]) {
+    val productPriceMap = Map("jajka" -> 6.99, "chleb" -> 3.5, "sok" -> 4.99, "jabłka" -> 3.05, "gruszki" -> 4.99, "ser" -> 23.99)
+    val newProductPriceMap = productPriceMap map (t => decreasePrice(t._1, t._2))
+    println(newProductPriceMap)
+  }
+
+  def decreasePrice(product: String, price: Double): (String, Double) = {
+    val newPrice = "%.2f".format(price*0.9).toDouble
+    product -> newPrice
+  }
+}
